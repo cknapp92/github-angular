be6b45a4e01e777430b3d880967dc5cb6ae1e581
Conrad Knapp <conrad.knapp@bc.edu>
2017-10-06 22:21:25 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ProfileComponent } from './components/profile.component'
 import { AppComponent }  from './app.component';
+import { GithubService } from './services/github.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, HttpModule],
+  declarations: [ AppComponent, ProfileComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [GithubService]
 })
 export class AppModule { }
